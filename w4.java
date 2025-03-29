bcca7b58d308e979aeff091eb412964f23e8bf3a
YarelyJ <154484018+YarelyJ@users.noreply.github.com>
2025-03-29 00:30:49 +0000

    Add files via upload

---
+++ w4.java
@@ -0,0 +1,13 @@
+package com.tesji.formulariotda;
+
+public class w4 {
+    public static void main(String[] args) {
+        short x;
+        x=2;
+        while (x<=100){
+            System.out.print(x);
+            System.out.print("\n");
+            x= (short) (x+2);
+        }
+    }
+}
